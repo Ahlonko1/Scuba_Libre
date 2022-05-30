59285deb951aad77b79b244f5bbb78de55c7b28c
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-05-30 17:54:20 +0200

    app and offer policies

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,23 @@
 class OffersController < ApplicationController
+
+  def index
+  end
+
+  def show
+    authorize @offer
+  end
+
+  def new
+    @offer = Offer.new
+    authorize @offer # Add this line
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    authorize @offer # Add this line
+  end
+
+
+
 end
