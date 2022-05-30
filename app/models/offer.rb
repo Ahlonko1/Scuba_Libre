e65f8b8c99074097329835204a0e563df7e40a52
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-05-30 22:06:32 +0200

    validations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,8 @@
 class Offer < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many_attached :photos
+  validates :name, :category, :duration, :unit_duration, :price, :currency, :level, :briefing, presence: true
+  validates :price, numericality: { only_integer: true }
+  validates_associated :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+
+  has_many :offers, dependent: :destroy
+  has_many :bookings
+  has_many :user_associations
+  has_many :associations, through: :user_associations
+  has_many :user_associations
+  has_many :certifications, through: :user_certifications
+  has_one_attached :avatar
+
+  # Renvoie les offres que j'ai reservées
+  has_many :booked_offers, through: :bookings, source: :offer
+
+  # renvoie les offres qu'on m'a reservées
+  has_many :booking_requested, through: :bookings, source: :bookings
+  validates :first_name, :last_name, presence: true
+  has_one_attached :avatar
+
 end
