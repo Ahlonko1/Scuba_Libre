d3c55b4c3adf95bb332b4a78a4318b3399bfe407
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-05-30 22:31:50 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :offers do
+    resources :bookings, only: %i[new create destroy]
+  end
+
+  resources :bookings, only: :index do
+    member do
+      patch :accepted
+      patch :denied
+    end
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
