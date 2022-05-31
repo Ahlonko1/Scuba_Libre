03b16b7c876879f567bb6eae983fca895ac367d4
Gauthier Nadaud <gnadaud@gmail.com>
2022-05-31 11:58:35 +0200

    pro controller + change column name teacher to pro dans users

---
+++ db/migrate/20220531095211_rename_teacher_column_to_pro_in_users.rb
@@ -0,0 +1,5 @@
+class RenameTeacherColumnToProInUsers < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :users, :teacher, :pro
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_30_144215) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_31_095211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,10 +89,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_144215) do
     t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
-    t.boolean "teacher"
+    t.boolean "pro"
     t.string "languages", default: [], array: true
     t.text "bio"
     t.string "location"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
