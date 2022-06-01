f6ae402a5521eec922b989eec45c0e42725e6473
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-06-01 11:34:38 +0200

    prerequisites dives and included to offers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_31_203345) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_01_093244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,6 +79,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_31_203345) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "currency"
+    t.text "prerequisites"
+    t.text "included"
+    t.integer "dives"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
