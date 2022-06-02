76d47fd2a98e08d4d2cd36fdd122ff9e9612a46f
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-06-02 15:46:43 +0200

    dashboard

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,17 +2,26 @@ class Booking < ApplicationRecord
   belongs_to :offer
   belongs_to :user
 
-  after_create :set_price, :set_currency
+  after_create :set_price, :set_currency, :set_location
 
   scope :pending, -> { where(status: "pending") }
   scope :accepted, -> { where(status: "accepted") }
   scope :denied, -> { where(status: "denied") }
 
+  validates :start_at, presence: true
+
   def set_price
     self.amount = offer.price
+    save
   end
 
   def set_currency
     self.currency = offer.currency
+    save
+  end
+
+  def set_location
+    self.location = offer.user.location
+    save
   end
 end
