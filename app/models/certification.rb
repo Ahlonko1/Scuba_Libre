512992911b36ca30ecfe7ca87960bcb42c6e3e25
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-06-03 14:42:36 +0200

    dependant_destroy_fix

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -1,5 +1,5 @@
 class Certification < ApplicationRecord
-  belongs_to :organism, dependent: :destroy
-  has_many :user_certifications
+  belongs_to :organism
+  has_many :user_certifications, dependent: :destroy
   has_many :users, through: :user_certifications
 end
--- app/models/organism.rb
+++ app/models/organism.rb
@@ -1,5 +1,5 @@
 class Organism < ApplicationRecord
-  has_many :certifications
-  has_many :user_associations
+  has_many :certifications, dependent: :destroy
+  has_many :user_associations, dependent: :destroy
   has_many :users, through: :user_associations
 end
